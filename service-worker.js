c055ede45b7f32963ce458a85c524c28d2877897
juanselvan361-create <juanselvan361@gmail.com>
2026-03-06 12:47:41 -0600

    Rename cache and update service worker logic

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,35 @@
-const CACHE_NAME = "app-ductos-v1";
+const CACHE_NAME="eventos-app-v1"
 
-const urlsToCache = [
+const urlsToCache=[
 "/",
 "/index.html"
-];
+]
 
-self.addEventListener("install", event => {
+self.addEventListener("install",event=>{
 
 event.waitUntil(
 
 caches.open(CACHE_NAME)
-.then(cache => {
-return cache.addAll(urlsToCache);
+.then(cache=>{
+return cache.addAll(urlsToCache)
 })
 
-);
+)
 
-});
+})
 
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch",event=>{
 
 event.respondWith(
 
 caches.match(event.request)
-.then(response => {
+.then(response=>{
 
-return response || fetch(event.request);
+return response || fetch(event.request)
 
 })
 
-);
+)
 
-});
+})
